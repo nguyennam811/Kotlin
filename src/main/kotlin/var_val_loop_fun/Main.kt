5b337f0b7366e05a1955187056d502ca768c17aa
Nam Nguyen <vannam08112002@gmail.com>
2025-03-11 16:57:41 +0700

    loop in kotlin

--- src/main/kotlin/var_val_loop_fun/Main.kt
+++ src/main/kotlin/var_val_loop_fun/Main.kt
@@ -3,6 +3,7 @@ package var_val_loop_fun
 fun main(args: Array<String>) {
     println("hello")
 
+    //variable
     var x = 10
     var y = x + 10
     println("Gia tri cua y la $y")
@@ -11,6 +12,74 @@ fun main(args: Array<String>) {
     println("a=$a")
     var b: Float = a.toFloat()
     println("b=$b")
-    var c: Float
+
+    var text = "a"
+
+    var c: Char = 'a'
+    println("char c = $c")
+//    var text1: Char = text
+
+    var text2 = "abc"
+    println("text2: ${text2[1]}")
+
+    var myArray: Array<Long> = arrayOf(1, 2, 3)
+    var testArray:Int = myArray[2].toInt()
+    println("testArray: $testArray")
+
+    // val (value) là hằng số, ko thể thay đổi , ko đc sử dụng lại
+//    val d = 15
+//    d = 10
+
+    println("Mời nhập 1 số")
+    var enterInt: Int = readLine()!!.toInt()
+    // !! tức là không được null
+    if (enterInt % 2 == 0) {
+        println("số chẵn")
+    } else {
+        println("số lẻ")
+    }
+
+    val day = 1
+    when (day) {
+        2 -> println("monday")
+        3 -> println("friday")
+        4 -> println("sunday")
+        5 -> println("planks")
+        6 -> println("tidy")
+        7 -> println("tidy")
+        else -> println("ngày sai")
+    }
+
+
+    val testFor = 5
+    for (i in 1..testFor) {
+        println("testFor .. $i")
+    }
+
+    for (i in 1 until testFor) {
+        println("testFor until $i")
+    }
+
+    for (i in testFor downTo  1) {
+        println("testFor downTo $i")
+    }
+
+    val array01 = arrayOf("một", 2 , 'c', 2.5)
+    for (i in array01) {
+        println("testArray $i")
+    }
+
+
+    var itemWhile = 1
+    while (itemWhile < 10) {
+        println("test while $itemWhile")
+        itemWhile++
+    }
+
+    var itemDoWhile = 1
+    do {
+        println("test do while $itemDoWhile")
+        itemDoWhile++
+    } while (itemDoWhile <= 0)
 }
 
